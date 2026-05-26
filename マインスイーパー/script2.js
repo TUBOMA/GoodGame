e15f1a82237dae4da8682397fcb5cf4f08513111
TUBOMA <144991260+TUBOMA@users.noreply.github.com>
2026-05-26 12:06:04 +0900

    コイン追加修正3

--- マインスイーパー/script2.js
+++ マインスイーパー/script2.js
@@ -26,11 +26,17 @@ function updateUI() {
 
 updateUI();
 
+
 // ===== 敵撃破 =====
 function defeatEnemy() {
-  /*if (typeof GameSystem !== 'undefined') {
-    GameSystem.addCoins(100); // 数字は一旦100統一で 調整は後々
-  }*/
+
+  // GameSystemのコイン追加
+  if (typeof GameSystem !== 'undefined') {
+    GameSystem.addCoins(100); // 数字は一旦100統一
+  }
+
+  // ローカル用コイン
+  gainCoins(100);
 
   // 撃破時に一瞬小さくする演出
   enemyEl.style.transform = "scale(0)";
@@ -41,8 +47,14 @@ function defeatEnemy() {
   // インフレ
   maxHp = Math.floor(maxHp * 1.2 + 10);
   hp = maxHp;
+
   updateUI();
-  gainCoins(100);
+}
+
+// ===== コイン加算 =====
+function gainCoins(amount) {
+  coin += amount;
+  updateUI();
 }
 
 // ===== 攻撃処理（共通） =====
